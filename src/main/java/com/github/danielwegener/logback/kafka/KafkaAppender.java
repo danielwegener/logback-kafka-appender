e730aa8fc23d6933b2dc4bd249895718b028ab06
Daniel Wegener <daniel@wegener.me>
2015-03-12 20:44:49 +0100

    +do never ever log a log message from any logger with the name org.apache.kafka.*. This closes #7

--- src/main/java/com/github/danielwegener/logback/kafka/KafkaAppender.java
+++ src/main/java/com/github/danielwegener/logback/kafka/KafkaAppender.java
@@ -6,4 +6,13 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
  * See <a href="https://github.com/danielwegener/logback-kafka-appender">logback-kafka-appender at github</a>
  */
 public class KafkaAppender extends KafkaAppenderBase<ILoggingEvent> {
+
+    private static final String KAFKA_LOGGER_PREFIX = "org.apache.kafka";
+
+    @Override
+    public void doAppend(ILoggingEvent eventObject) {
+        if (eventObject.getLoggerName().startsWith(KAFKA_LOGGER_PREFIX)) return;
+        super.doAppend(eventObject);
+    }
+
 }
