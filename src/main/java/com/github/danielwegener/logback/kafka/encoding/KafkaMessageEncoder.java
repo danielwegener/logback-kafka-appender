0ff0911d0594d4d947057ef7e6c41da338aba0ee
Daniel Wegener <daniel@wegener.me>
2015-03-12 17:12:21 +0100

    = adds documentation for delivery strategies and add javadoc to public interfaces.

--- src/main/java/com/github/danielwegener/logback/kafka/encoding/KafkaMessageEncoder.java
+++ src/main/java/com/github/danielwegener/logback/kafka/encoding/KafkaMessageEncoder.java
@@ -1,5 +1,10 @@
 package com.github.danielwegener.logback.kafka.encoding;
 
+/**
+ * An Encoder that is able to take an {@code E} and return a {byte[]}.
+ * This Encoder should naturally be referential transparent.
+ * @param <E>
+ */
 public interface KafkaMessageEncoder<E> {
 
     byte[] doEncode(E loggingEvent);
--- src/main/java/com/github/danielwegener/logback/kafka/encoding/KafkaMessageEncoderBase.java
+++ src/main/java/com/github/danielwegener/logback/kafka/encoding/KafkaMessageEncoderBase.java
@@ -3,6 +3,9 @@ package com.github.danielwegener.logback.kafka.encoding;
 import ch.qos.logback.core.spi.ContextAwareBase;
 import ch.qos.logback.core.spi.LifeCycle;
 
+/**
+ * A base class for {@link KafkaMessageEncoder}'s that are {@link ContextAwareBase} and have a {@link LifeCycle}
+ */
 public abstract class KafkaMessageEncoderBase<E> extends ContextAwareBase implements KafkaMessageEncoder<E>, LifeCycle {
 
     private boolean started = false;
